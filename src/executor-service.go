7b3a3fe773bc8c9cd25b9037e423b21b48dbb2d0
srigiriraju <srigiriraju@yahoo-inc.com>
2018-10-14 18:31:29 -0700

    fix: updated code with executor service implementation with runnalbe and callbale interface

--- src/executor-service.go
+++ src/executor-service.go
@@ -6,11 +6,16 @@ import (
 
 type ExecutorService interface {
     execute()
-    submit() chan
+    submit() chan string
 }
 
 type Runnable interface {
     run()
+}
+
+type Callable interface {
+    call()
+    getChan() chan string
 }
 
 type CustomJobExecutor struct {
@@ -18,20 +23,40 @@ type CustomJobExecutor struct {
 }
 
 type MyJob struct {
+    c chan string
     value string
 }
 
 func (executor CustomJobExecutor) execute(r Runnable) {
-    r.run()
+    go r.run()
+}
+
+func (executor CustomJobExecutor) submit(c Callable) (chan string) {
+    go c.call()
+    return c.getChan()
 }
 
 func (job MyJob) run() {
     log.Println(job.value)
 }
 
+func (job MyJob) call() {
+    job.c <- job.value
+}
+
+func (job MyJob) getChan() (chan string){
+    return job.c
+}
+
 
 func main() {
     var executor CustomJobExecutor = CustomJobExecutor{}
-    var myJob MyJob = MyJob{value: "testing"}
+    var myJob MyJob = MyJob{value: "testing", c: make(chan string)}
     executor.execute(myJob)
+    c := executor.submit(myJob)
+    result := <- c
+    log.Println(">>>>>>")
+    log.Println(result)
+    //infinite loop
+    for {}
 }
